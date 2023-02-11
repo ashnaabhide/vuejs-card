0893176128149cd5c002010efee1a1ef9ba19aef
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-02-11 17:49:50 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,110 +1,121 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-<!-- My custom code -->
-<button class="outsideBtn" v-on:click="duplicate">Duplicate</button>
-<button class="outsideBtn" v-on:click="background">Background</button>
-<button class="outsideBtn" v-on:click="heading">Heading</button>
-<button class="outsideBtn" v-on:click="deleter">Delete</button>
-
-<div class="card" v-on:mouseover="hoover">
-  <h1 class="title">Chad of Cyber IST</h1>
-
-  <img class="img" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe">
-  
-<div class="textbox">
-  <details>
-    <summary class="haxbtn">Details</summary>
-    <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
-  </details>
-
-  </div>
-</div>
-
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+import HelloWorld from './components/HelloWorld.vue'
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<script>
-
-export default {
-  methods: {
-    hoover() {
-      document.querySelectorAll(".card").forEach((item) => {
-        if(!item.classList.contains("hoverCard")){
-          item.classList.add("hoverCard");
-          console.log(item);
-        }
-        else{
-          item.classList.remove("hoverCard");
-          console.log(item);
-        }
-      });
-    },
-    
-    duplicate() {
-      const cloneCard = document.querySelector(".card").cloneNode(true);
-      document.body.appendChild(cloneCard);
-      console.log(cloneCard);    
-    },
-
-    background() {
-      document.querySelectorAll(".card").forEach((item) => {
-        if(!item.classList.contains("basic")){
-          item.classList.add("basic");
-          console.log(item);
-        }
-        else{
-          item.classList.remove("basic");
-          console.log(item);
-        }
-      });
-    },
-
-    heading() {
-      document.querySelectorAll(".title").forEach((item) => {   
-        if(item.innerHTML=="something else"){
-          item.innerHTML="Chad of Cyber IST";
-        }
-        else{
-          item.innerHTML="something else";
-        }
-      });
-    },
-
-    //Kinda borked rn
-    deleter(){
-      document.querySelector(".card:last-child").remove();
-      console.log(card);
-    }
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-</script>
-
-<style scoped>
-a {
-  color: #42b983;
+.card{
+  border-radius: 24px;
+  border: 3px solid #041E42;
+  padding: 8px;
+  width: 400px;
+  display: block;
+  margin: auto;
+  position: relative;
+  overflow: hidden;
+  box-shadow: 0px 8px 24px #999;
+  background-color: lightgrey;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
 }
+.textbox{
+  font-size: 15px;
+  margin-left: 112px;
+  text-align: right;
+  border-radius: 24px;
+}
+.description{
+  text-shadow: -1px 1px 2px #FFFFFF,
+				  1px 1px 2px #FFFFFF,
+				  1px -1px 0 #FFFFFF,
+				  -1px -1px 0 #FFFFFF;
+}
+.haxbtn{
+  display:inline-block;
+  text-align: center;
+  color: white;
+  background-color: #041E42;
+  border-radius: 5px;
+  font-size: 20px;
+  width: 120px;
+  height: 32px;
+  box-shadow: 0px 10px 24px #999;
+  border: 1px solid #041E42;
+  visibility: visible;
+  padding-top: 10px;
+  margin-right: 32px;
+}
+.haxbtn:hover{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.haxbtn:focus{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.title{
+  color: white;
+  font-size: 24px;
+  text-shadow: -1px 1px 2px #000,
+				  1px 1px 2px #000,
+				  1px -1px 0 #000,
+				  -1px -1px 0 #000;
+}
+.img{
+  width: 200px;
+  border-radius: 24px;
+  box-shadow: 0px 10px 24px #999;
+  float: left;
+}
+.outsideBtn{
+  color: white;
+  background-color: #041E42;
+  border-radius: 5px;
+  font-size: 20px;
+  width: 120px;
+  height: 50px;
+  box-shadow: 0px 10px 24px #999;
+  border: 1px solid #041E42;
+  margin-right: 32px;
+  margin-bottom: 16px;
+  display: inline-block;
+}
+.outsideBtn:hover{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.outsideBtn:focus{
+  color: #e0e0e0;
+  background-color: #07377a;
+  box-shadow: 0px 10px 24px white;
+  border: 1px solid #07377a;
+}
+.basic{
+  background-color: hotpink;
+}
+.hoverCard{
+  border: 3px solid purple;
+  box-shadow: 0px 8px 24px yellow;
+}
+input:checked + label + .description { display:none; }
 </style>
